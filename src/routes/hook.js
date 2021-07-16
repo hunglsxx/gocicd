61c25f6144fe5edba1d13a2671f1d783b1272aed
Lê Sĩ Hưng <hungls@gostream.vn>
2021-07-16 11:16:54 +0700

    Worker test

--- src/routes/hook.js
+++ src/routes/hook.js
@@ -1,42 +1,47 @@
 var express = require('express');
 var router = express.Router();
 const queue = require('fastq').promise(worker, 1);
-const { execSync } = require('child_process');
+const { spawnSync } = require('child_process');
 
 async function worker(arg) {
-    console.log("In worker", arg);
-    let cd = execSync(`cd ${arg.dir}`);
-    return cd;
+    try {
+        console.log("In worker", arg);
+        let cd = spawnSync('git', ['pull origin', arg.git_branch], { cwd: arg.dir });
+        return cd;
+    } catch (error) {
+        return error;
+    }
 }
 
 /* POST home page. */
 router.post('/', async function (req, res, next) {
-    var projects = req.app.get('projects');
-    console.log("POST", req.body);
-    console.log("Project config:", projects);
-    if (req.body
-        && req.body.ref
-        && req.body.project
-        && req.body.project.git_http_url) {
-        var refPath = req.body.ref.split('/');
-        var gitBranch = refPath[refPath.length - 1];
-        var cicdConfig;
-        console.log("Git branch", gitBranch, "Git http url:", req.body.project.git_http_url);
-        for (var i in projects) {
-            console.log("Project config:", projects[i]);
-            if (projects[i].git_http_url == req.body.project.git_http_url
-                && projects[i].git_branch == gitBranch) {
-                cicdConfig = projects[i];
-                console.log("Config:", cicdConfig);
-                break;
+    try {
+        var projects = req.app.get('projects');
+        // console.log(req.body);
+        if (req.body
+            && req.body.ref
+            && req.body.project
+            && req.body.project.git_http_url) {
+            var refPath = req.body.ref.split('/');
+            var gitBranch = refPath[refPath.length - 1];
+            var cicdConfig;
+            for (var i in projects) {
+                if (projects[i].git_http_url == req.body.project.git_http_url
+                    && projects[i].git_branch == gitBranch) {
+                    cicdConfig = projects[i];
+                    break;
+                }
+            }
+            if (cicdConfig) {
+                const result = await queue.push(cicdConfig);
+                console.log("Out worker:", result.stdout.toString());
             }
         }
-        if (cicdConfig) {
-            const result = await queue.push(cicdConfig);
-            console.log("Out worker:", result);
-        }
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+        return res.send({ 'message': 'Received' });
     }
-    res.render('index', { title: 'Hook' });
 });
 
 
